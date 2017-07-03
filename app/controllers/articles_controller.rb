e4fd489e6f15d4818d258aa243cb667bbf6b37c8
kageyama <kageyama@mediba.jp>
2017-07-03 14:53:25 +0900

    add aws setting to .env

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
   def index
+    credentials = Aws::Credentials.new ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_KEY']
     s3 = Aws::S3::Client.new(
       region: 'ap-notrheast-1',
-      credentials: Aws::Credentials.new('test', 'test'),
-      endpoint: 'http://localhost:4568',
+      credentials: credentials,
+      endpoint: ENV['S3_ENDPOINT'],
       force_path_style: true
     )
 
